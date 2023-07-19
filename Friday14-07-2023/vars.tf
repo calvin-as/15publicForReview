c77145628e86eeaa06b1c5ce5f9aecaa78034878
Calvin Asmus <calvinasmus@MacBook-Air-von-Calvin.local>
2023-07-19 23:48:27 +0200

    fix var.block for Subnets

--- Friday14-07-2023/vars.tf
+++ Friday14-07-2023/vars.tf
@@ -20,18 +20,7 @@ variable "private_cidr2"{
     default = "10.0.2.0/24"
 }
 
-#CIDR block for the VPC, Subnets
 variable "VPC_cidr_block" {
   description = "CIDR block for the VPC"
   default     = "10.0.0.0/16"
-}
-
-variable "subnet1_cidr_block" {
-  description = "CIDR block for subnet 1"
-  default     = "10.0.1.0/24"
-}
-
-variable "subnet2_cidr_block" {
-  description = "CIDR block for subnet 2"
-  default     = "10.0.2.0/24"
 }
