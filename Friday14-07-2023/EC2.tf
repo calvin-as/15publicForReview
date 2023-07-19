0ca2e158e4271c6d0f8d6b54d52ee43cf2a6e2fd
Calvin Asmus <calvinasmus@MacBook-Air-von-Calvin.local>
2023-07-20 01:55:52 +0200

    delete comments and add comments

--- Friday14-07-2023/EC2.tf
+++ Friday14-07-2023/EC2.tf
@@ -15,14 +15,6 @@ data "aws_ami" "latest_amazon_linux" {
 
   owners = ["amazon"] # Amazon
 }
-/*
-data "aws_ami" "amzLinux" {
-        most_recent = true
-        owners = ["amazon"]
-    }
-#in ressource aws_instance: ami                         = data.aws_ami.amzLinux.id
-*/
-
 
 ###Create EC2
 resource "aws_instance" "deham6demo"{
@@ -34,59 +26,5 @@ resource "aws_instance" "deham6demo"{
     tags = {
         Name = "terraform18_ec2_for_public_subnet1_az1"
     }
-    user_data              = "${file("user-data.sh")}"
+    user_data              = file("user-data.sh")
     }
-
-#Create user data --> should save in another file 
-/*
-  user_data = <<-EOF
-#!/bin/bash
-
-# System-Update
-sudo yum update -y
-
-# Installation von Apache, MariaDB, PHP und zusätzlichen Paketen
-sudo yum install -y httpd mariadb-server php php-mysqlnd unzip
-
-# EPEL und Remi Repository für PHP 7.4 hinzufügen
-sudo amazon-linux-extras install epel
-sudo yum install https://rpms.remirepo.net/enterprise/remi-release-8.rpm
-
-# PHP 7.4 aktivieren und benötigte PHP-Pakete installieren
-sudo amazon-linux-extras enable php7.4
-sudo yum clean metadata
-sudo yum install php-cli php-pdo php-fpm php-json php-mysqlnd php php-{mbstring,json,xml,mysqlnd}
-
-# Starten und Aktivieren von Apache und MariaDB
-sudo systemctl start httpd
-sudo systemctl enable httpd
-sudo systemctl start mariadb
-sudo systemctl enable mariadb
-
-# WordPress herunterladen und konfigurieren
-cd /var/www/html
-sudo curl -LO https://wordpress.org/latest.zip
-sudo unzip latest.zip
-sudo mv -f wordpress/* ./
-sudo rm -rf wordpress latest.zip
-sudo chown -R apache:apache /var/www/html
-
-# Konfiguration von MariaDB für WordPress
-sudo mysql -e "CREATE DATABASE wordpress;"
-sudo mysql -e "CREATE USER 'wpuser'@'localhost' IDENTIFIED BY 'wppassword';"
-sudo mysql -e "GRANT ALL PRIVILEGES ON wordpress.* TO 'wpuser'@'localhost';"
-sudo mysql -e "FLUSH PRIVILEGES;"
-
-sudo yum update -y
-
-# Neustart von Apache um alle Änderungen zu übernehmen
-sudo systemctl restart httpd
-
-# Überprüfung der PHP-Version
-php -v
-sudo yum update -y
-
-  EOF
-
-
-*/
--- Friday14-07-2023/output.tf
+++ Friday14-07-2023/output.tf
@@ -16,9 +16,52 @@ output "security_group"{
 }
 
 #IP-Adress for the Webserver
-output "public_ip" {
+output "public_ip_of_first_EC2" {
   value = aws_instance.deham6demo.public_ip
 }
+
+output "name_of_first_EC2" {
+  value = aws_instance.deham6demo.tags.Name
+}
+
+output "ip_and_name_of_first_EC2" {
+  value = "${aws_instance.deham6demo.public_ip} ${aws_instance.deham6demo.tags.Name}"
+}
+
+/*
+# Load Balancer details
+output "application_load_balancer_arn" {
+  value = aws_lb.devVPC_application_load_balancer.arn
+}
+output "application_load_balancer_id" {
+  value = aws_lb.devVPC_application_load_balancer.id
+}
+
+# Auto Scaling Group details
+output "auto_scaling_group_id" {
+  value = aws_autoscaling_group.devVPC_auto_scaling_group.id
+}
+output "auto_scaling_group_arn" {
+  value = aws_autoscaling_group.devVPC_auto_scaling_group.arn
+}
+
+# Launch Configuration details
+output "launch_configuration_id" {
+  value = aws_launch_configuration.devVPC_launch_configuration.id
+}
+output "launch_configuration_name" {
+  value = aws_launch_configuration.devVPC_launch_configuration.name
+}
+
+# Target Group details
+output "target_group_arn" {
+  value = aws_lb_target_group.devVPC_target_group.arn
+}
+output "target_group_id" {
+  value = aws_lb_target_group.devVPC_target_group.id
+}
+*/
+
 
 /*
 output "packer_ami"{
