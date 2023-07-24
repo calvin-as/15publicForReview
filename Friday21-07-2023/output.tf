30e1d3e1ecce7a4dd5c606f97a441b112a324309
Calvin Asmus <calvinasmus@MacBook-Air-von-Calvin.local>
2023-07-24 14:30:16 +0200

    add outputs

--- Friday21-07-2023/output.tf
+++ Friday21-07-2023/output.tf
@@ -15,11 +15,45 @@ output "security_group"{
     value = aws_security_group.devVPC_sg_allow_ssh_http # devVPC_sg_allow_http.id
 }
 
-/* Friday Challange 14.7.2023
-#IP-Adress for the Webserver
+# Friday Challange 14.7.2023
+#not tested
 output "public_ip_of_first_EC2" {
   value = aws_instance.deham6demo.public_ip
 }
+
+output "aws_instance" {
+  value = aws_instance.deham6demo.id
+}
+
+output "aws_autoscaling_group" {
+  value = aws_autoscaling_group.deham6demo_asg.id
+}
+
+output "aws_autoscaling_policy" {
+  value = aws_autoscaling_policy.scale_up.id
+}
+
+output "aws_lb" {
+  value = aws_lb.devVPC_application_load_balancer.id
+}
+
+output "aws_lb_target_group" {
+  value = aws_lb_target_group.devVPC_target_group.id
+}
+
+output "aws_lb_listener" {
+  value = aws_lb_listener.devVPC_listener.id
+}
+
+output "aws_launch_template" {
+  value = aws_launch_template.deham6demo.id
+}
+
+
+
+/*
+#IP-Adress for the Webserver
+
 
 output "name_of_first_EC2" {
   value = aws_instance.deham6demo.tags.Name
