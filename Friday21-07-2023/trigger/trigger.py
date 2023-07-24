642e0e1a60d2c82034deec5a8422e64bf6076d4b
Calvin Asmus <calvinasmus@MacBook-Air-von-Calvin.local>
2023-07-24 11:29:59 +0200

    add Documentation and trigger

---
+++ Friday21-07-2023/trigger/trigger.py
@@ -0,0 +1,28 @@
+##trigger Auto-Scale Group 
+###replace ip & type in terminal to start python3 trigger.py
+import time
+import requests
+from multiprocessing import Pool
+
+# The URLs of your load balancers
+urls = ["http://xx.xx.xx.xx"] #,.. 
+
+# The number of parallel requests you want to send
+num_requests = 6
+
+# The time between requests (in seconds)
+delay = 0.1
+
+def send_request(i):
+    url = urls[i % len(urls)]
+    while True:
+        try:
+            response = requests.get(url)
+            print(f"Request to {url}, Status Code: {response.status_code}")
+        except Exception as e:
+            print(f"Request to {url}, failed with {e}")
+        time.sleep(delay)
+
+if __name__ == "__main__":
+    with Pool(num_requests) as p:
+        p.map(send_request, range(num_requests))
